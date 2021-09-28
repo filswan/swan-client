c9c481abd7fef4195380cea4fd8fa90208e00e0b
gma <gma@nbai.io>
2021-09-28 14:19:28 -0400

    Update max price

--- task_sender/swan_task_sender.py
+++ task_sender/swan_task_sender.py
@@ -472,11 +472,13 @@ def send_autobid_deal(deals,miner_id,task_info,config_path,out_dir):
         piece_cid = _deal["piece_cid"]
         file_size = _deal["file_size"]
         prices = get_miner_price(miner_id)
-        real_price = -1
+        real_price = None
         if prices:
             if task_info["type"]:
                 if task_info["type"] == 'verified':
                     real_price = prices['verified_price']
+                else:
+                    real_price = prices['price']
             else:
                 real_price = prices['price']
         else:
